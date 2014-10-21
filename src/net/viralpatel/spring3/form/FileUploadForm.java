14c0bfc43093a60119715f93388ead77fd2aa48a
Wilson M. Vergara <wvergara@exist.com>
2014-10-21 23:10:09 +0800

    first commit

---
+++ src/net/viralpatel/spring3/form/FileUploadForm.java
@@ -0,0 +1,18 @@
+package net.viralpatel.spring3.form;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadForm {
+
+	private List<MultipartFile> files;
+
+	public List<MultipartFile> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<MultipartFile> files) {
+		this.files = files;
+	}
+}
